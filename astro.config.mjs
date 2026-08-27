fefa46415b7863ffeaf698ebb01ef842c9fac2b9
HitsuK0 <wellinger.chloe@proton.me>
2026-08-27 20:19:53 +0200

    feat: ajout du btn télécharger CV

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  // Si tu déploies sur https://ton-pseudo.github.io/nom-du-repo/,
-  // décommente et adapte la ligne suivante :
-  // base: "/nom-du-repo",
+  site: "https://HitsuK0.github.io",
+  base: "/portfolio",
   server: {
-    host: true, // équivalent de --host, utile pour Docker
+    host: true, 
   },
 });
